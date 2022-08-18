21160119a167065a8c2e24fd77686db9a734b1de
Joey Binas <joseph.t.binas@gmail.com>
2022-08-17 18:00:47 -0700

    added asset caching functionality, StaleWhileRev

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { StaleWhileRevalidate, CacheFirst } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -27,4 +27,23 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  // define callback function, which filters requests we want to cache
+  //in this example, this will include our JS and CSS files
+  ({ request }) => ['style', 'script', 'worker'].includes( request.destination ),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      // cache status of 0 or 200; 
+      // expiration plugin will cache responses to max age of 30 days
+
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      // new ExpirationPlugin({
+      //   maxEntries: 60,
+      //   maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      // }),
+    ],
+  })
+);
